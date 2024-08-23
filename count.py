f86b1e09f38174414da8a620dffc1c6b92b16c7c
edramos-lab <edgar.rene.ramos.acosta@uabc.edu.mx>
2024-08-22 18:47:51 -0700

    train folder updated

--- count.py
+++ count.py
@@ -9,7 +9,7 @@ def count_images_per_folder(directory):
                 image_count[folder] = image_count.get(folder, 0) + 1
     return image_count
 
-directory = 'C:/Users/edgar/OneDrive/Documentos/PhD/MLOPS/SMAP-Recognition/CustomDataset/resized_train (copy)'  # Replace with the actual directory path
+directory = 'C:/Users/edgar/OneDrive/Documentos/PhD/MLOPS/SMAP-Recognition/CustomDataset/train'  # Replace with the actual directory path
 image_count = count_images_per_folder(directory)
 for folder, count in image_count.items():
     print(f"Folder '{folder}' contains {count} image(s).")
